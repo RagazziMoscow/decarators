3712dc98b95c6dbc24e7c02a08c795b3a27011be
Ilya <iliya5376@mail.com>
2017-10-17 17:02:55 +0300

    feature(decorators.py) add @ignore_exceptions decorator

--- decorators.py
+++ decorators.py
@@ -4,7 +4,7 @@ import json
 
 
 def log_duration(func):
-    def wrapped(*args, **kwargs):
+    def wrapper():
         func_name = func.__name__
 
         start = time.time()
@@ -12,13 +12,14 @@ def log_duration(func):
         end = time.time()
         duration = end - start
 
-        duration = round(duration * 1000, 2)
-        duration = str(duration) + ' мс'
+        duration = round(duration * 10 ** 6, 2)
+        duration = str(duration) + ' мкс'
         print('Функция {0} {2} {1}'.format(color(func_name, fg='green', style='bold'),
-                                           color(duration, fg='red', style='bold'),
+                                           color(duration, fg='red',
+                                                 style='bold'),
                                            color('->', style='bold')))
         return result
-    return wrapped
+    return wrapper
 
 
 def to_json(func):
@@ -29,6 +30,18 @@ def to_json(func):
         return result
     return wrapped
 
+
+def ignore_exceptions(exception_class):
+    def exception_decorator(func):
+        def wrapper():
+            try:
+                func()
+            except exception_class:
+                print(color('Обнаружена ошибка', fg='red'))
+                return None
+        return wrapper
+
+    return exception_decorator
 
 if __name__ == '__main__':
     pass
